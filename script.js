641971777c321381f34face093e3c4c2917aeee9
Clare-Boniface <clarelgarfield@hotmail.com>
2023-07-09 12:00:06 +0100

    add hamburger menu for smaller screens

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const menu = document.getElementById("menu");
 const hamburgerIcon = document.getElementById("hamburger-menu-icon");
 const closeBtn = document.getElementById("close-btn");
+const viewPostBtn = document.getElementById("view-more-btn");
+const extraPosts = document.getElementById("extra-posts");
 
 hamburgerIcon.addEventListener("click", function () {
   console.log("clicked");
@@ -9,4 +11,40 @@ hamburgerIcon.addEventListener("click", function () {
 
 closeBtn.addEventListener("click", function () {
   menu.classList.remove("show");
+});
+
+/*viewPostBtn.addEventListener("click", function () {
+  if (viewPostBtn.textContent === "View More") {
+    extraPosts.classList.remove("hidden-posts");
+    viewPostBtn.textContent = "View Less";
+  } else {
+    extraPosts.classList.add("hidden-posts");
+    viewPostBtn.textContent = "View More";
+  }
+});*/
+
+/*const viewMoreText = "View More";
+const viewLessText = "View Less";
+
+viewPostBtn.addEventListener("click", function () {
+  const viewMoreText = "View More";
+  const viewLessText = "View Less";
+  extraPosts.classList.toggle("hidden-posts");
+  viewPostBtn.textContent =
+    viewPostBtn.textContent === viewMoreText ? viewLessText : viewMoreText;
+});*/
+
+/*viewPostBtn.addEventListener("click", function () {
+  extraPosts.classList.toggle("hidden-posts");
+  viewPostBtn.textContent =
+    viewPostBtn.textContent === "View More"
+      ? (viewPostBtn.textContent = "View Less")
+      : (viewPostBtn.textContent = "View More");
+});*/
+
+viewPostBtn.addEventListener("click", function () {
+  extraPosts.classList.toggle("hidden-posts");
+  viewPostBtn.textContent.trim() === "View More"
+    ? (viewPostBtn.textContent = "View Less")
+    : (viewPostBtn.textContent = "View More");
 });
