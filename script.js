dc87de7343c06f42eb9268b2bff5d61d8378e2f7
Clare-Boniface <clarelgarfield@hotmail.com>
2023-07-16 17:24:29 +0100

    update styling on CSS

--- script.js
+++ script.js
@@ -13,35 +13,6 @@ closeBtn.addEventListener("click", function () {
   menu.classList.remove("show");
 });
 
-/*viewPostBtn.addEventListener("click", function () {
-  if (viewPostBtn.textContent === "View More") {
-    extraPosts.classList.remove("hidden-posts");
-    viewPostBtn.textContent = "View Less";
-  } else {
-    extraPosts.classList.add("hidden-posts");
-    viewPostBtn.textContent = "View More";
-  }
-});*/
-
-/*const viewMoreText = "View More";
-const viewLessText = "View Less";
-
-viewPostBtn.addEventListener("click", function () {
-  const viewMoreText = "View More";
-  const viewLessText = "View Less";
-  extraPosts.classList.toggle("hidden-posts");
-  viewPostBtn.textContent =
-    viewPostBtn.textContent === viewMoreText ? viewLessText : viewMoreText;
-});*/
-
-/*viewPostBtn.addEventListener("click", function () {
-  extraPosts.classList.toggle("hidden-posts");
-  viewPostBtn.textContent =
-    viewPostBtn.textContent === "View More"
-      ? (viewPostBtn.textContent = "View Less")
-      : (viewPostBtn.textContent = "View More");
-});*/
-
 viewPostBtn.addEventListener("click", function () {
   extraPosts.classList.toggle("hidden-posts");
   viewPostBtn.textContent.trim() === "View More"
